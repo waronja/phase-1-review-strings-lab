4ce91084bd10a0ee42cb4d0f790cdae19a138efd
unknown <crazsich@gmail.com>
2022-06-06 20:42:47 +0300

    Strings lab assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser = 'Thaiya Waronja';
+const welcomeMessage = 'Welcome to Flatbook ' + currentUser + "!!" ;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//Printing out the short Greeting 
+const firstInitial = currentUser[0];
+const shortGreeting = 'Welcome, ' + firstInitial + '!';
+
+console.log (shortGreeting);
